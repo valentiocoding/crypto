732b499b35fa9ed049915710b33288f32c23c564
valentio <valentiocoding@gmail.com>
2025-02-12 14:27:26 +0700

    a

--- input.py
+++ input.py
@@ -20,13 +20,13 @@ if category == "Top Up Toko Crypto":
     col2.metric("Saldo Toko Crypto", f"{data['saldo_toko'].sum():,.2f}", border=True)
     with st.form("my_form", clear_on_submit=True):
         selectedcategory = "Top Up Toko Crypto"
+        date = st.date_input("Date")
         rupiah = st.number_input("Rupiah", format="%.10f")
         saldo = st.number_input("Saldo Toko Crypto", format="%.10f")
         
         button = st.form_submit_button("Submit")
         
         if button:
-            date = datetime.datetime.now()
             
             input_data(date,rupiah,saldo,0,0,0,"",0, selectedcategory)
             st.success("Berhasil input.")
@@ -40,6 +40,7 @@ elif category == "Buy USDT on Toko Crypto":
     st.metric("Average Buy Rate USDT",f"{usdt_rate:,.2f}", border=True )
     with st.form("my_form", clear_on_submit=True):
         selectedcategory = "Buy USDT on Toko Crypto"
+        date = st.date_input("Date")
         saldo = st.number_input("Saldo Toko Crypto", format="%.10f")
         rate = st.number_input("Rate", format="%.10f")
         usdt = st.number_input("USDT", format="%.10f")
@@ -47,7 +48,6 @@ elif category == "Buy USDT on Toko Crypto":
         button = st.form_submit_button("Submit")
         
         if button:
-            date = datetime.datetime.now()
             saldo = -1 * saldo
             input_data(date,0,saldo,rate,usdt,0,"",0, selectedcategory)
             st.success("Berhasil input.")
@@ -59,13 +59,13 @@ elif category == "Transfer USDT to Binance":
     col2.metric("USDT Binance", f"{data['usdt_binance'].sum():,.2f}", border=True)
     with st.form("my_form", clear_on_submit=True):
         selectedcategory = "Transfer USDT to Binance"
+        date = st.date_input("Date")
         usdt = st.number_input("USDT", format="%.10f")
         usdt_binance = st.number_input("USDT Binance", format="%.10f")
         
         button = st.form_submit_button("Submit")
         
         if button:
-            date = datetime.datetime.now()
             usdt = -1 * usdt
             input_data(date,0,0,0,usdt,usdt_binance,"",0, selectedcategory)
             st.success("Berhasil input.")
@@ -85,6 +85,7 @@ elif category == "Buy Coin on Binance":
 
 
     with st.form("my_form", clear_on_submit=True):
+        date = st.date_input("Date")
         coin = st.text_input("Coin")
         rate = st.number_input("Rate", format="%.10f")
         selectedcategory = "Buy Coin on Binance"
@@ -94,7 +95,6 @@ elif category == "Buy Coin on Binance":
         button = st.form_submit_button("Submit")
         
         if button:
-            date = datetime.datetime.now()
             usdt_binance = -1 * usdt_binance
             input_data(date,0,0,rate , 0, usdt_binance, coin, QtyCoin, selectedcategory)
             st.success("Berhasil input.")
@@ -116,6 +116,7 @@ elif category == "Sell Coin on Binance":
 
     with st.form("my_form", clear_on_submit=True):
         selectedcategory = "Sell Coin on Binance"
+        date = st.date_input("Date")
         coin = st.text_input("Coin")
         QtyCoin = st.number_input("QtyCoin", format="%.10f")
         rate = st.number_input("Rate", format="%.10f")
@@ -124,7 +125,6 @@ elif category == "Sell Coin on Binance":
         button = st.form_submit_button("Submit")
         
         if button:
-            date = datetime.datetime.now()
             QtyCoin = -1 * QtyCoin
             input_data(date,0,0,rate , 0, usdt_binance, coin, QtyCoin, selectedcategory)
             st.success("Berhasil input.")
@@ -138,13 +138,13 @@ elif category == "Transfer USDT to Toko Crypto":
     col2.metric("USDT Toko Crypto", f"{data['usdt'].sum():,.2f}", border=True)
     with st.form("my_form", clear_on_submit=True):
         selectedcategory = "Transfer USDT to Toko Crypto"
+        date = st.date_input("Date")
         usdt_binance = st.number_input("USDT on Binance", format="%.10f")
         usdt = st.number_input("USDT Toko Crypto", format="%.10f")
         
         button = st.form_submit_button("Submit")
         
         if button:
-            date = datetime.datetime.now()
             usdt_binance = -1 * usdt_binance
             input_data(date,0,0,0,usdt,usdt_binance,"",0, selectedcategory)
             st.success("Berhasil input.")
@@ -153,6 +153,7 @@ elif category == "Transfer USDT to Toko Crypto":
 elif category == "Sell USDT on Toko Crypto":
     with st.form("my_form", clear_on_submit=True):
         selectedcategory = "Sell USDT on Toko Crypto"
+        date = st.date_input("Date")
         rate = st.number_input("Rate", format="%.10f")
         usdt = st.number_input("USDT", format="%.10f")
         saldo = st.number_input("Saldo Toko Crypto", format="%.10f")
@@ -160,7 +161,6 @@ elif category == "Sell USDT on Toko Crypto":
         button = st.form_submit_button("Submit")
         
         if button:
-            date = datetime.datetime.now()
             usdt = -1 * usdt
             input_data(date,0,saldo,rate,usdt,0,"",0, selectedcategory)
             st.success("Berhasil input.")
@@ -173,13 +173,13 @@ elif category == "Transfer to Bank Account":
     col2.metric("Rupiah", f"{data['rupiah'].sum():,.2f}", border=True)
     with st.form("my_form", clear_on_submit=True):
         selectedcategory = "Transfer to Bank Account"
+        date = st.date_input("Date")
         saldo = st.number_input("Saldo Toko Crypto", format="%.10f")
         rupiah = st.number_input("Rupiah", format="%.10f")
         
         button = st.form_submit_button("Submit")
         
         if button:
-            date = datetime.datetime.now()
             saldo = -1 * saldo
             input_data(date,rupiah,saldo,0,0,0,"",0, selectedcategory)
             st.success("Berhasil input.")
